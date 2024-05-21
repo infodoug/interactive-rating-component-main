e09f23daf7dbe61aad93749e86ee9c59b5da2710
infodoug <douglasrafael.info@gmail.com>
2024-05-21 18:08:10 -0300

    funcao salvarnota

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function Botao() {
+function SalvarNota(nota) {
+    let notaAtual = nota;
+    let notaApresentada = document.getElementById('nota-apresentada');
+    notaApresentada.textContent = notaAtual;
+}
+
+function BotaoSubmit() {
     let ratingpage = document.getElementById('ratingpage');
     ratingpage.remove();
 
