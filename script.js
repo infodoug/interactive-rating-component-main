78f2920e872c76f28d8705b891643349fde8fea7
infodoug <douglasrafael.info@gmail.com>
2024-05-21 21:54:18 -0300

    ratingpage

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-function SalvarNota(nota) {
-    let notaAtual = nota;
+function SalvarNota(botaoClicado, nota) {
+    let botoesNota = document.querySelectorAll('.botao-nota');
+    botoesNota.forEach(botao => {
+        if (botao !== botaoClicado) {
+            botao.classList.remove('clicado');
+        }
+    }); 
+
+    botaoClicado.classList.add('clicado');
+
     let notaApresentada = document.getElementById('nota-apresentada');
-    notaApresentada.textContent = notaAtual;
+        notaApresentada.textContent = nota;
 }
 
 function BotaoSubmit() {
-    let ratingpage = document.getElementById('ratingpage');
-    ratingpage.remove();
+    let erro = document.getElementById('erro');
+    try {
+        let notaApresentada = document.getElementById('nota-apresentada').textContent;
 
-    let thankspage = document.getElementById('thankspage');
-    thankspage.classList.add('visible');
+        if (notaApresentada === '') {
+            throw new Error('Por favor, selecione uma nota antes de enviar.');
+        }
+
+        let ratingpage = document.getElementById('ratingpage');
+        ratingpage.remove();
+
+        let thankspage = document.getElementById('thankspage');
+        thankspage.classList.add('visible');
+    } catch (error) {
+        erro.textContent = error.message;
+    }
 }
